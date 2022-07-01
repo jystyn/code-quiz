cd96340a9a838e1d49c560a6d5154868c47efc7d
joostyn <justyn.helgeson@gmail.com>
2022-06-30 21:48:40 -0500

    initial commit

---
+++ assets/quiz.js
@@ -0,0 +1,137 @@
+var questions = [
+    {
+        question: 'What is like the skeleton of the website?',
+        answers: [
+            {text: 'html', correct: true },
+            {text: 'css', correct: false },
+            {text: 'javascript', correct: false },
+            {text: 'c++', correct: false }
+        ]
+    },
+    {
+        question: 'Which of these styles the website?',
+        answers: [
+            {text: 'html', correct: false },
+            {text: 'css', correct: true },
+            {text: 'javascript', correct: false },
+            {text: 'c++', correct: false}
+        ]
+    },
+    {
+        question: 'What makes a website interactive?',
+        answers: [
+            {text: 'html', correct: false },
+            {text: 'css', correct: false },
+            {text: 'javascript', correct: true },
+            {text: 'c++', correct: false}
+        ]
+    },
+    {
+        question: 'Which of these is the "and" logical operator?',
+        answers: [
+            {text: '&&', correct: true },
+            {text: '||', correct: false },
+            {text: '>=', correct: false },
+            {text: '!', correct: false}
+        ]
+    },
+    {
+        question: 'Which of these is the "or" logical operator?',
+        answers: [
+            {text: '&&', correct: false },
+            {text: '||', correct: true },
+            {text: '>=', correct: false },
+            {text: '!', correct: false}
+        ]
+    }
+];
+    //timerEl allows us to display the countdown timer in the top right corner
+    var timerEl = document.getElementById('timer');
+    // creating elements for us to modify different parts of our base HTML
+    var questionEl = document.getElementById('question');
+    // var answerBtnEls = document.querySelector('.answer');
+    var msgEl = document.getElementById('message');
+    var startBtnEl = document.getElementById('startBtn');
+    var wrapperEl = document.querySelector('.wrapper');
+    var btnEl = document.querySelector('.btn');
+    // create variables to shuffle questions
+    var shuffledQuestions, currentQuestionIndex;
+    var timeLeft = 60;
+    function startTimer() {
+       var timer = setInterval(function(){
+            if (timeLeft > 0) {
+                timeLeft--;
+                timerEl.innerHTML = 'Time Left: ' + timeLeft + 's';;
+            } else if (timeLeft === 0) {
+                timerEl.innerHTML = 'Times Up!';
+                clearInterval(timer);
+            }
+        },1000); 
+    }
+    function start(){
+        startTimer();
+        // When we press 'Start Quiz' it changes the page to get us ready for questions
+        msgEl.innerHTML = '';
+        startBtnEl.remove();
+        shuffledQuestions = questions.sort(() => Math.random() - .5 );
+        currentQuestionIndex = 0;
+        setNextQuestion();
+    }    
+
+    function setNextQuestion() {
+        showQuestion(shuffledQuestions[currentQuestionIndex]);
+    }
+
+    function showQuestion(question) {
+        questionEl.innerText = question.question;
+        question.answers.forEach(answer => {
+            var button = document.createElement('button');
+            button.innerText = answer.text;
+            button.classList.add('btn');
+            wrapperEl.appendChild(button);
+            // selectAnswer();
+        })
+    }
+
+    function selectAnswer(){
+        btnEl.addEventListener('click', function(){
+        var userAnswer = this.innerText;
+        console.log(userAnswer);
+
+        })
+
+    }
+
+    function resetState() {
+
+    }
+
+    // function selectAnswer(e) {
+    //     // var selectedButton = e.target;
+    //     // var correct = selectedButton.dataset.correct;
+    //     // setStatusClass(document.body, correct);
+    //     // Array.from(wrapperEl.children).forEach(button => {
+    //     //     setStatusClass(button, button.dataset.correct);
+    //     // })
+    // }
+
+    // function setStatusClass(element, correct) {
+    //     // clearStatusClass(element);
+    //     if (correct) {
+    //         element.classList.add('correct');
+
+    //     } else if (!correct) {
+    //         element.classList.add('wrong');
+    //     }
+    // }
+
+    // function clearStatusClass(element) {
+
+    // }
+
+
+
+    // btn1.innerHTML = questions[0].answers[0];
+    // btn2.innerHTML = questions[0].answers[1];
+    // btn3.innerHTML = questions[0].answers[2];
+    // btn4.innerHTML = questions[0].answers[3];
