93eafce30622020d354fa2559594e902ec9307f0
joostyn <justyn.helgeson@gmail.com>
2022-07-04 13:26:28 -0500

    cleaned up code, updated README

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
-oof... Live to fight another day I guess.
+This assignment was to create a coding quiz using Javascript. 
 
-This project is a quiz using javascript. 
+When the user clicks the start button the quiz and timer start.
+Everytime the user answers a question, a new question is presented.
+When the user answers a question correctly they recieve 10 points.
+When the user answers a question incorrectly, they lose 10 seconds.
+When the timer runs out or the user answers all the questions the game is over.
+When the game is over, they are asked for thier initials and they are added to a leaderboard.
 
 https://jystyn.github.io/code-quiz/
-![code-quiz screenshot](./assets/code-quiz-screenshot.png)
+![code-quiz screenshot 1](./assets/code-quiz-screenshot-1.png)
+![code-quiz screenshot 2](./assets/code-quiz-screenshot-2.png)
+![code-quiz screenshot 3](./assets/code-quiz-screenshot-3.png)
+![code-quiz screenshot 4](./assets/code-quiz-screenshot-4.png)
+![code-quiz screenshot 5](./assets/code-quiz-screenshot-5.png)
--- assets/script.js
+++ assets/script.js
@@ -57,6 +57,7 @@ var questions = [
     var startBtnEl = document.querySelector('#start-btn');
     var wrapperEl = document.querySelector('.wrapper');
     var btnWrapperEL = document.querySelector('.btn-wrapper');
+    var AnsValidationEL = document.querySelector('#ans-validation');
     var finalScorePageEl = document.querySelector('#final-score-page');
     var finalScoreEl = document.querySelector('#final-score');
     var submitScoreBtnEl = document.querySelector('#submit-score-btn');
@@ -68,6 +69,7 @@ var questions = [
     var currentQuestionIndex = 0;
     var userScore = 0;
     var timeLeft = 60;
+    
     // Clicking events on the home page
     viewLeaderboardEL.addEventListener("click", goToLeaderboard);
     startBtnEl.addEventListener('click', startQuiz);
@@ -76,16 +78,18 @@ var questions = [
     function startQuiz(){
         msgEl.innerHTML = '';
         startBtnEl.remove();
+        //this shuffles the questions
         shuffledQuestions = questions.sort(() => Math.random() - .5 );
         startTimer();
         showQuestion(shuffledQuestions[currentQuestionIndex]);
     }   
+
     // Timer function 
     function startTimer() {
         var timer = setInterval(function(){
              if (timeLeft > 0) {
                  timeLeft--;
-                 timerEl.innerHTML = 'Time Left: ' + timeLeft + 's';;
+                 timerEl.innerHTML = `Time left: ${timeLeft}s`;
              } else if (timeLeft === 0) {
                  timerEl.innerHTML = 'Times Up!';
                  clearInterval(timer);
@@ -93,6 +97,7 @@ var questions = [
              }
          },1000); 
      }
+
     // This populates the first question and it's corresponding answers
     function showQuestion(question) {
         questionEl.innerText = question.question;
@@ -104,14 +109,23 @@ var questions = [
             button.addEventListener("click", showNextQuestion);
         })
     }
+
     // This shows a new question everytime you submit an answer
     function showNextQuestion(){
+        AnsValidationEL.innerHTML = '';
         if (this.innerText == questions[currentQuestionIndex].correct) {
         userScore+=10;
-        //message correct
+        //Show message if correct
+        var p = document.createElement('p');
+        p.innerText = 'Correct! 10 points!'
+        AnsValidationEL.appendChild(p);
         } else {
         timeLeft-=10;
-        //message wrong
+        //show message if wrong
+        var p = document.createElement('p');
+        p.innerText = 'Incorrect! 10 second penalty.'
+        AnsValidationEL.appendChild(p);
+
         }
         currentQuestionIndex++;
         if (currentQuestionIndex >= questions.length) {
@@ -121,14 +135,16 @@ var questions = [
             showQuestion(shuffledQuestions[currentQuestionIndex]);
         }
     }
+
     // This shows the user their final score and allows them to enter their initials
     function finalScorePage(){
-        wrapperEl.innerHTML = '';
+        wrapperEl.innerHTML = '<h1>Game Over!</h1>';
         finalScorePageEl.style.display = "flex";
         finalScoreEl.innerText = userScore;
         submitScoreBtnEl.addEventListener("click", showLeaderboard);
 
     }
+
     // This shows the leaderboard after the user enters initials
     function showLeaderboard(event){
         event.preventDefault();
@@ -140,36 +156,41 @@ var questions = [
         addHighScore();
     }
 
+    // This function is what adds the users score to the leaderboard
     function addHighScore(){
         var listHighScores = JSON.parse(localStorage.getItem('listHighScores')) || [];
         var userInitials = document.querySelector("#initials").value;
-        // Order high scores from greatest to least
-        var ordererdLdrbrd = listHighScores.sort((a, b) => (b.score > a.score) ? 1 : -1);
-        
         var highScore = {
             initials : userInitials,
             score: userScore,
         }
+        // This adds a new score to the High Scores list in local storage
         listHighScores.push(highScore);
         localStorage.setItem("listHighScores", JSON.stringify(listHighScores));
+        // Order high scores from greatest to least
+        var ordererdLdrbrd = listHighScores.sort((a, b) => (b.score > a.score) ? 1 : -1);
 
+        // This creates the ordered list on the Leaderboard
         for (var i = 0; i < ordererdLdrbrd.length; i++) {
             var li = document.createElement('li');
             li.innerText = `${ordererdLdrbrd[i].initials}: ${ordererdLdrbrd[i].score}`;
-            
             leaderboardListEl.appendChild(li);
         }
     }
 
+    // This sends the user to the leaderboard when they click "View Leaderboard"
     function goToLeaderboard() {
         wrapperEl.innerHTML = '';
         leaderboardEl.style.display = "flex";
         finalScorePageEl.style.display = "none";
+        // This gets rid of the ability to click "View Leaderboard" since we're already there
         viewLeaderboardEL.removeEventListener("click", goToLeaderboard);
+        
         var leaderboardList = JSON.parse(localStorage.getItem('listHighScores')); 
+
         for (var i = 0; i < leaderboardList.length; i++) {
             var li = document.createElement('li');
-            li.innerText = `${leaderboardList[i].initials}: ${leaderboardList[i].score}`;
+            li.innerText = `${leaderboardList[i].initials} ${leaderboardList[i].score}`;
             leaderboardListEl.appendChild(li);
         }
     }
